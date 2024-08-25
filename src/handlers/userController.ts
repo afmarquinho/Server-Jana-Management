09cfed09408b38c231885dfbb03dc2e5cfa78045
Marquinhos <fernandez9029@gmail.com>
2024-08-25 13:35:56 -0500

    Se corrige log del manejo de error en la login form

--- src/handlers/userController.ts
+++ src/handlers/userController.ts
@@ -196,6 +196,7 @@ export const login = async (req: Request, res: Response) => {
       data: payload,
       token,
     });
+    
   } catch (error) {
     console.error("Error en la autenticación:", error);
     res.status(500).json({ error: "Error en la autenticación" });
